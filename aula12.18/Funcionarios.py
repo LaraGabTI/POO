957e058dbc875af5b8681e334860e263db65738c
Lara Gabryella <laragabryella7@gmail.com>
2023-12-19 17:06:27 -0300

    aula do dia 18.12

--- aula12.18/Funcionarios.py
+++ aula12.18/Funcionarios.py
@@ -3,7 +3,8 @@
 class Funcionarios:
     def __init__(self, nome, salario):
         self.nome = nome
-        self.salario = 0.0
+        self.salario = salario
+        self.ponto = False
     
     def get_nome(self):
         return self.nome 
@@ -11,21 +12,30 @@ class Funcionarios:
     def get_salario(self):
         return self.salario
     
+    def bater_ponto_entrada(self):
+        self.ponto = True
+        return f'Bateu o ponto pra entrar'
     
+    def bater_ponto_saida(self):
+        self.ponto = False
+        return f'Bateu o ponto pra sair'
     
 class Gerente(Funcionarios):
-    def __init(self, nome, idade, salario):
-        super().__init__(nome, idade)
-        self.salario = salario
-
+    def __init__(self, nome, salario, idade):
+        super().__init__(nome, salario)
+        self.idade = idade
+        
     def __str__(self):  #funcao que imprime tudo
         return f'Nome: { self.nome }, Idade: { self.idade } e Salario: { self.salario }.'
 
 class Desenvolvedor(Funcionarios):
-    def __init(self, nome, idade, salario):
+    def __init__(self, nome, idade, salario):
         super().__init__(nome, idade)
         self.salario = salario
 
     def __str__(self):  #funcao que imprime tudo
         return f'Nome: { self.nome }, Idade: { self.idade } e Salario: { self.salario }.'
     
+paulo = Gerente('Paulo', 1200, 22)
+print(paulo)
+print(paulo.bater_ponto_entrada())
--- aula12.18/Pessoa.py
+++ aula12.18/Pessoa.py
@@ -2,17 +2,17 @@ class Pessoa: #super classe
     def __init__(self, nome, idade): #atributos em comum de prof e aluno
         self.nome = nome
         self.idade = idade
-        self.__cpf = None
+        self.__cpf = None #cpf privado
     
     def get_cpf(self):
         return self.__cpf
     
     def set_cpf(self, meu_cpf):
-        self.__cpf = meu_cpf
+        self.__cpf = meu_cpf #privado
 
 class Professor(Pessoa): #classe filho
     def __init__(self, nome, idade, salario, disciplina, cpf): #atributos 
-        super().__init__(nome, idade) #recebe da classe super
+        super().__init__(nome, idade) #recebe da classe super, n consegue o privado
         self.salario = salario
         self.disciplina = disciplina
         self.cpf = super().set_cpf(cpf) #recebe da classe super
@@ -21,9 +21,26 @@ class Professor(Pessoa): #classe filho
         return f'Nome: { self.nome }, Idade: { self.idade } e CPF: { self.get_cpf() } Salario: { self.salario }, Disciplina: { self.disciplina }.'
     
 class Aluno(Pessoa):
-    pass
+    def __init__(self, nome, idade, cpf):
+        super().__init__(nome, idade)        
+        self.cpf = super().set_cpf(cpf)
+        self.notasGeral = 0
+
+    def notas(self, n1, n2, n3, n4):
+        self.notasGeral = ((n1 + n2 + n3 + n4) / 4)
+    
+    def get_notas(self):
+        return self.notasGeral
+
+    
+    def __str__(self):  #funcao que imprime tudo
+        return f'Nome: { self.nome }, Idade: { self.idade } e CPF: { self.get_cpf() } notas Getal: { self.notasGeral} .'
 
 paulo = Professor('Paulo Junior', 29, 1400.00, 'Backend', 1234567890)
 lara = Professor('Lara', 22, 1500, 'Matematica', 8252918360)
 print(paulo) #chamado assim por conta da funcao string
 print(lara)
+
+paula = Aluno('Paula', 22, 828282)
+paula.notas(8, 9, 10, 5)
+print(paula)
