1edd7ce6c727d7136ecec38ef39acf724c30e838
Lara Gabryella <laragabryella7@gmail.com>
2023-12-18 21:48:08 -0300

    aula 12.18

--- Bomba_de_Gasolina.py
+++
@@ -1,9 +0,0 @@
-class Bomba_de_Gasolina:
-    def __init__(self, qnttCombustivel, tipoCombustivel):
-        self.qnttCombustivel = qnttCombustivel
-        self.tipoCombustivel = tipoCombustivel
-        self.valorLitro = 0
-        self.valorPagar = 0
-    
-    def escolha(self, tipoCombustivel, qnttCombustivel)
-        print('')
